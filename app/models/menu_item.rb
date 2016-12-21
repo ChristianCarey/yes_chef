ffc33875a7c034d12e8b789b7e53a1ebef074bc3
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-21 15:35:34 -0700

    Navigation and menu items

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -2,7 +2,7 @@ class MenuItem < ApplicationRecord
   has_many :menu_selections
   has_many :menus, through: :menu_selections
 
-  belongs_to :chef, -> { where(:role, 'chef') }, class_name: "User", foreign_key: :user_id
+  belongs_to :chef, class_name: "User", foreign_key: :user_id
 
   validates_presence_of :name, :description
   validates_uniqueness_of :name, scope: [:user_id]
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
   has_many :menus
 
+  has_many :menu_items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
