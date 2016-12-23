ae60934f1484db0f3f2d476e413e98f11f7e2df1
Alexa <alexa.anderson@outlook.com>
2016-12-23 00:26:24 -0600

    change ingredients user association to chef

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ApplicationRecord
-  belongs_to :user
+  belongs_to :chef, class_name: 'User', foreign_key: :user_id
   has_many :ingredients_menu_items, inverse_of: :ingredient
   has_many :menu_items, through: :ingredients_menu_items, inverse_of: :ingredients
   validates_presence_of :name
