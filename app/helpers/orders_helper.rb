9ace33300873c969a3a3e86a0daabba983a17f97
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-22 23:40:59 -0700

    add menu item image to order form

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,4 +4,9 @@ module OrdersHelper
     date.strftime("%A, %b %e")
   end
 
+  def render_menu_item_img(menu_item)
+    if menu_item.image.exists?
+      "#{image_tag menu_item.image.url(:thumb)} ".html_safe
+    end
+  end
 end
