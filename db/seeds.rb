bcef891eb871d39aff7957b2740a5bd21b3ca59a
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-23 10:48:48 -0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Ingredient.destroy_all
 OrderItem.destroy_all
 Order.destroy_all
 MenuItem.destroy_all
-Menu.destroy_all  
+Menu.destroy_all
 Profile.destroy_all
 
 
@@ -14,29 +14,29 @@ CATEGORIES = %w(Breakfast Lunch Dinner Vegan Gluten-free Vegetarian Paleo Freega
 
 puts "Creating chef..."
 User.create(email: "a@a.com",
-            password: "password",
-            password_confirmation: "password",
-            first_name: Faker::Name.first_name,
-            last_name: Faker::Name.last_name,
-            role: "chef")
+password: "password",
+password_confirmation: "password",
+first_name: Faker::Name.first_name,
+last_name: Faker::Name.last_name,
+role: "chef")
 
 puts "Creating customers..."
 
 User.create(email: "customer@example.com",
-            password: "password",
-            password_confirmation: "password",
-            first_name: Faker::Name.first_name,
-            last_name: Faker::Name.last_name,
-            role: "customer",
-            chef: User.where(role: "chef").sample)
+password: "password",
+password_confirmation: "password",
+first_name: Faker::Name.first_name,
+last_name: Faker::Name.last_name,
+role: "customer",
+chef: User.where(role: "chef").sample)
 5.times do
   User.create(email: Faker::Internet.email,
-              password: "password",
-              password_confirmation: "password",
-              first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              role: "customer",
-              chef: User.where(role: "chef").sample)
+  password: "password",
+  password_confirmation: "password",
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  role: "customer",
+  chef: User.where(role: "chef").sample)
 end
 
 puts "Creating categories..."
@@ -45,9 +45,9 @@ CATEGORIES.each do |category|
 end
 
 puts "Creating ingredients..."
-10.times do 
+10.times do
   Ingredient.create(chef: User.where(role: "chef").first,
-                    name: Faker::Hipster.word)
+  name: Faker::Hipster.word)
 end
 
 puts "Creating menu items..."
@@ -60,35 +60,36 @@ puts "Creating menu items..."
 
   ingredients = Ingredient.all
 
-  4.times do 
+  4.times do
     ingredient = ingredients.shuffle.pop
     menu_item.ingredients << ingredient unless menu_item.ingredients.include?(ingredient)
   end
 end
 
-puts "Creating menus..."
 
+puts "Creating menus..."
 User.where(role: "chef").each do |chef|
   3.times do |n|
-    chef.menus.create(order_deadline: Faker::Time.between(Date.today, 5.days.from_now),
-                      completion_date: (6 + n).days.from_now)
-  end
-end
+    menu = chef.menus.build(order_deadline: Faker::Time.between(Date.today, 5.days.from_now),
+                            completion_date: (6 + n).days.from_now)
+    menu_items = []
 
-puts "Creating menu selections and placing orders..."
-Menu.all.each do |menu|
-  rand(3..5).times do
-    begin
-      menu_item = MenuItem.all.sample
-    end until !menu.menu_items.include?(menu_item)
+    puts "Creating menu selections and placing orders..."
+    rand(3..5).times do
+      begin
+        menu_item = MenuItem.all.sample.id
+      end until !menu_items.include?(menu_item)
 
-    menu.menu_items << menu_item
-  end
+      menu_items << menu_item
 
-  10.times do
-    chef = User.where(role: "chef").sample
-    Order.create(menu: chef.menus.sample,
-                 customer: chef.customers.sample)
+    end
+    menu.menu_item_ids = menu_items
+    menu.save!
+
+    10.times do
+      Order.create(menu: chef.menus.sample,
+      customer: chef.customers.sample)
+    end
   end
 end
 
@@ -96,8 +97,6 @@ puts "Adding items to orders..."
 Order.all.each do |order|
   rand(1..order.menu.menu_items.size).times do
     order.order_items.create(menu_item: order.menu.menu_items.sample,
-                                  quantity: rand(1..3))
+    quantity: rand(1..3))
   end
 end
-
-
