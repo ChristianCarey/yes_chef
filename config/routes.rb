c30e4c2f63fb376823734c35597b6c6b803d8fd7
Alexa <alexa.anderson@outlook.com>
2016-12-22 19:21:38 -0600

    Add profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for  :users, :controllers => { registrations: 'registrations' }
-  resources   :users
+  resources   :users do
+    resource :profile
+  end
   resources   :menu_items
   resources   :menus do
     resources :orders
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222183759) do
+ActiveRecord::Schema.define(version: 20161223003833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,17 @@ ActiveRecord::Schema.define(version: 20161222183759) do
     t.index ["menu_id", "customer_id"], name: "index_orders_on_menu_id_and_customer_id", using: :btree
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "telephone"
+    t.string   "address"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -127,4 +138,5 @@ ActiveRecord::Schema.define(version: 20161222183759) do
   add_foreign_key "menu_item_orders", "orders"
   add_foreign_key "menu_selections", "menu_items"
   add_foreign_key "menu_selections", "menus"
+  add_foreign_key "profiles", "users"
 end
