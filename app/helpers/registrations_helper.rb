e90e046bab18b17983799ab4cabc34338ca8ce6b
Alexa <alexa.anderson@outlook.com>
2016-12-23 03:37:05 -0600

    some styles to sign up form

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,17 @@
 module RegistrationsHelper
+  def welcome_message(role)
+    if role == 'chef'
+      "Welcome, Chef!"
+    else
+      "Welcome to Yes, Chef!"
+    end
+  end
+
+  def welcome_tagline(role)
+    if role == 'chef'
+      "May your clients ever be hungry"
+    else
+      "You clearly have a finer taste in life"
+    end
+  end
 end
