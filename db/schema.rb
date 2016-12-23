1959b8acaba1cee3d9ac4c9e7ae5534b9382d923
Christian Carey <christianrcarey@gmail.com>
2016-12-22 22:34:26 -0500

    set up mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223003833) do
+ActiveRecord::Schema.define(version: 20161223030156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,21 @@ ActiveRecord::Schema.define(version: 20161223003833) do
     t.datetime "updated_at",   null: false
     t.index ["category_id"], name: "index_categories_menu_items_on_category_id", using: :btree
     t.index ["menu_item_id"], name: "index_categories_menu_items_on_menu_item_id", using: :btree
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
   end
 
   create_table "ingredients", force: :cascade do |t|
