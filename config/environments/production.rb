ec3ce6fd26d834219ba9e7dd7e8e4852a0732f38
Christian Carey <christianrcarey@gmail.com>
2016-12-23 11:18:20 -0500

    set up for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,20 @@ Rails.application.configure do
     }
   }
 
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = {
+    :host => 'https://yes-chef.herokuapp.com/',
+  }
+
   
   # Settings specified here will take precedence over those in config/application.rb.
 
